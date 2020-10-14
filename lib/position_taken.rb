9fbaa7aab1cbb2d2034c3f18e252c232f859899e
Damian Malysza <damian.malysza@gmail.com>
2020-10-14 00:37:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,index)
- if board[index] == "" || board[index] == " " || board[index] == nil
+  index = index.to_i - 1
+  if board[index] == "" || board[index] == " " || board[index] == nil
   return false
 end
