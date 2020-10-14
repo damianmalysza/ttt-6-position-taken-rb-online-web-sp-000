8853611eb79160098b28aa21da43bede7fba91cf
Damian Malysza <damian.malysza@gmail.com>
2020-10-14 00:41:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,6 +4,6 @@ def position_taken?(board,index)
   if board[index] == "X" || board[index] == "O"
     return true
   else
-    board[index] == "" || board[index] == " " || board[index] == nil
+    return board[index] == "" || board[index] == " " || board[index] == nil
   end
 end
